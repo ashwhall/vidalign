3c76468b4964c3196bbd78da6d87a5918303223e
Ash <ashwhall@gmail.com>
2023-04-12 10:33:36 +1000

    Unattended encode for whole directory

--- vidalign/encode_unattended.py
+++ vidalign/encode_unattended.py
@@ -1,18 +1,24 @@
 """
 A crude script to run encode tasks by loading a videoclip config file and printing progress to stdout.
+Run like:
+python3 vidalign/encode_unattended.py path/to/config.vc.json
+OR
+python3 vidalign/encode_unattended.py dir/containing/configs
 """
 
 
 from time import sleep
+import os
 from sys import argv
 from PySide6.QtWidgets import QApplication
 from vidalign.model import Model
 from vidalign.controllers import MainController
 from vidalign.views.main_view import MainView
 
+app = QApplication()
 
-def main(p):
-    app = QApplication()
+
+def run(p):
     model = Model()
     main_controller = MainController(model)
     main_view = MainView(model, main_controller)
@@ -35,12 +41,31 @@ def main(p):
         if len(model.encoding_stdout_lines) > 0:
             new_line = fmt_line(model.encoding_percentage, model.encoding_stdout_lines[-1])
             if new_line != last_line:
-                print(new_line, end='\r')
+                print(new_line, end='\r', flush=True)
                 last_line = new_line
         sleep(0.25)
     print('Finished!')
 
 
+def run_dir(p):
+    for f in os.listdir(p):
+        if f.endswith('.vc.json'):
+            print(f'Running {f}...')
+            run(os.path.join(p, f))
+
+
 if __name__ == '__main__':
     p = argv[1]
-    main(p)
+
+    if os.path.isdir(p):
+        should_run = None
+        while should_run not in ['y', 'n']:
+            should_run = input(
+                'You selected a directory - run for each *.vc.json file in this directory? (y/n) ')
+            should_run = should_run.lower()
+        if should_run == 'y':
+            run_dir(p)
+        else:
+            print('Exiting...')
+    else:
+        main(p)
